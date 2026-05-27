d27c398e62f6f3d3868dbc3297a073ed53e0490d
GodKode69 <knownasraghav@gmail.com>
2026-05-27 20:25:47 +0530

    added next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/GodKode69.github.io",
+  assetPrefix: "/GodKode69.github.io/",
 };
 
 export default nextConfig;
